bb251bf86bdf91cdea6c53cd63f2ce8592be372f
prateek1o1 <prateekchhimwal@gmail.com>
2023-03-04 08:07:38 +0530

    Milestone-2

---
+++ hadbackend/hadbackend/src/main/java/com/example/hadbackend/bean/OnConfirmResponse.java
@@ -0,0 +1,20 @@
+package com.example.hadbackend.bean;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class OnConfirmResponse {
+    private String requestId;
+    private String timestamp;
+    private ConfirmAuth auth;
+    private Err error;
+    private Resp resp;
+
+    public String getRequestid(){
+        return this.requestId;
+    }
+    public ConfirmAuth getAuth(){
+        return this.auth;
+    }
+
+}
