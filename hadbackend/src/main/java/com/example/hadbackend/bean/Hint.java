9dbbc4c367824c267c4f42ba01f77bbea89a397f
Prateek Chhimwal <47253656+prateek1o1@users.noreply.github.com>
2023-03-29 11:26:30 +0530

    Delete hadbackend directory

--- hadbackend/src/main/java/com/example/hadbackend/bean/Hint.java
+++
@@ -1,7 +0,0 @@
-package com.example.hadbackend.bean;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class Hint {
-}
